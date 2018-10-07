3ba57c467b63d2d9f60145b8fda9033726bc41b0
Dmitriy Shikhaleev <padavankun@gmail.com>
2018-10-07 20:27:48 +0300

    added rest of files

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Main from './Main';
+
+const App = () => (
+  <Main />
+);
+
+ReactDOM.render(<App />, document.getElementById('app'));
